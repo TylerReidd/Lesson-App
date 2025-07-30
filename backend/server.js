18f5ccaed43c521969f9ce556d4d5b88248a4398
TylerReidd <58642201+TylerReidd@users.noreply.github.com>
2025-07-30 09:16:06 -0400

    Update server

--- backend/server.js
+++ backend/server.js
@@ -21,17 +21,17 @@ app.use(express.json())
 const PORT = process.env.PORT || 5001
 
 const corsOptions = {
-  origin: 'http://tylerreidd.github.io', 
+  origin: 'https://tylerreidd.github.io', 
   credentials:true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', "OPTIONS"],
-  allowedHeaders: ["Content-type, 'Authorization"]
+  allowedHeaders: ['Content-type', 'Authorization']
 }
 
-app.use(cors({
-  origin: 'http://tylerreidd.github.io', 
-  credentials:true,
-  }
-));
+// app.use(cors({
+//   origin: 'http://tylerreidd.github.io', 
+//   credentials:true,
+//   }
+// ));
 
 app.use(cors(corsOptions))
 app.options('*', cors(corsOptions))
