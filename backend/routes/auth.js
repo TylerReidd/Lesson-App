83a490a274ce91d7614e7515541e0d0b5217eee7
TylerReidd <58642201+TylerReidd@users.noreply.github.com>
2025-07-30 12:27:50 -0400

    I hate cors

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -30,8 +30,8 @@ router.post('/login', async (req, res) => {
     
     res.cookie('token', token, {
                 httpOnly: true,
-                secure: false,
-                sameSite: 'lax',
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: 'none',
                 maxAge: 24*60*60*1000
               })
 
