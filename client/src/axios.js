c357c87155368e31eb3154924481dfc9bfe8d48b
TylerReidd <58642201+TylerReidd@users.noreply.github.com>
2025-07-30 09:18:31 -0400

    fix vite config

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
+const dev = import.meta.env.MODE === 'development';
+
 const instance = axios.create({
-  baseURL: '/api',
+  baseURL: dev 
+  ? '/api'
+  : 'https://lesson-app.onrender.com',
   withCredentials: true,
 })
 
