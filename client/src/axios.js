8e638d32ac1d739dee7ee7f5192d6b06022ee830
TylerReidd <58642201+TylerReidd@users.noreply.github.com>
2025-08-01 10:32:08 -0400

    axios adjustments, 404 fixing

--- client/src/axios.js
+++ client/src/axios.js
@@ -6,6 +6,16 @@ axios.defaults.baseURL = isDev
 ? '/api'
 : 'https://lesson-app-4pp6.onrender.com/api'
 
+axios.interceptors.response.use(
+  response => response, 
+  error => {
+    if (error.response?.status === 401) {
+      return Promise.resolve({data: {user: null}})
+    }
+    return Promise.reject(error)
+  }
+)
+
 axios.defaults.withCredentials = true
 
 export default axios
