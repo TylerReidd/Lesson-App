599cadcfa9fe3492bcd1ab260d100cbddddf7b16
TylerReidd <58642201+TylerReidd@users.noreply.github.com>
2025-08-01 10:11:19 -0400

    auth controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -22,8 +22,9 @@ export async function signup (req, res) {
     res
       .cookie('token', token, {
         httpOnly: true,
-        secure:false,
-        sameSite: 'lax',
+        secure:true,
+        sameSite: 'none',
+        path: '/',
         maxAge: 24 * 60 * 60 * 1000,
       })
       .status(201)
